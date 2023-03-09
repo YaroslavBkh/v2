d85d7341eadf333369cc1cf85c5d9f13e7d86cbd
Yaroslav <yaroslav.bkh@gmail.com>
2023-03-10 00:20:53 +0200

    feat: setup scss with global vars

---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import classes from "./Header.module.scss";
+
+const Header = () => {
+  return (
+    <header className={classes.header}>
+      modular classes.header with $color-global
+    </header>
+  );
+};
+
+export default Header;
